4dca82b76b6330d0f319d1c44842ea692c8acb45
Saqlain Shaikh <82815239+Saqlain96Shaikh@users.noreply.github.com>
2021-09-15 18:17:07 +0530

    Add files via upload

---
+++ TempConverter.java
@@ -0,0 +1,17 @@
+public class TempConverter{
+    public static void main(String[] args){
+        System.out.println("Sr. No\t\tTemperature(Celsius)\t\tTemperature(Fahrenheit)");
+        System.out.println(1+"\t\t"+0+"\t\t\t\t"+((0*9/5)+32));
+        System.out.println(2+"\t\t"+10+"\t\t\t\t"+((10*9/5)+32));
+        System.out.println(3+"\t\t"+20+"\t\t\t\t"+((20*9/5)+32));
+        System.out.println(4+"\t\t"+30+"\t\t\t\t"+((30*9/5)+32));
+        System.out.println(5+"\t\t"+40+"\t\t\t\t"+((40*9/5)+32));
+        System.out.println(6+"\t\t"+50+"\t\t\t\t"+((50*9/5)+32));
+        System.out.println(7+"\t\t"+60+"\t\t\t\t"+((60*9/5)+32));
+        System.out.println(8+"\t\t"+70+"\t\t\t\t"+((70*9/5)+32));
+        System.out.println(9+"\t\t"+80+"\t\t\t\t"+((80*9/5)+32));
+        System.out.println(10+"\t\t"+90+"\t\t\t\t"+((90*9/5)+32));
+        System.out.println(11+"\t\t"+100+"\t\t\t\t"+((100*9/5)+32));
+
+    }
+}
